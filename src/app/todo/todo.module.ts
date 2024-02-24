7407dca4fb7942c8b11c056f6bfbc1f5a02e04ce
Matheus Araujo Morais <matheus.araujo.morais@outlook.com>
2024-02-24 18:25:13 -0300

    feat: create modules

---
+++ src/app/todo/todo.module.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { TodoController } from './todo.controller';
+
+@Module({
+  providers: [TodoService],
+  controllers: [TodoController],
+  exports: [TodoService],
+})
+export class TodoModule {}
