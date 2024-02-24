dbf31f54ac2567d3f433d9cb740c3a11f40d645c
Matheus Araujo Morais <matheus.araujo.morais@outlook.com>
2024-02-24 18:08:02 -0300

    feat: configure MariaDB database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mariadb',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: configService.get('DB_PORT', 3306),
+        username: configService.get('DB_USERNAME', 'root'),
+        password: configService.get('DB_PASSWORD', '123'),
+        database: configService.get('DB_NAME', 'todo'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
